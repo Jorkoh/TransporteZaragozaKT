c95921932b07e8aab4dba77fad394709bca88211
Jorkoh <kohru95@gmail.com>
2019-01-25 00:18:28 +0100

    Initial commit

---
+++ app/src/main/java/com/jorkoh/transportezaragozakt/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.jorkoh.transportezaragozakt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemReselectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_favorites -> {
+                Log.d("TestingStuff", "Favorites")
+                val favoritesFragment = FavoritesFragment.newInstance()
+                openFragment(favoritesFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_map -> {
+                Log.d("TestingStuff", "Map")
+                val mapFragment = MapFragment.newInstance()
+                openFragment(mapFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_search -> {
+                Log.d("TestingStuff", "Search")
+                val searchFragment = SearchFragment.newInstance()
+                openFragment(searchFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_more -> {
+                Log.d("TestingStuff", "More")
+                val moreFragment = MoreFragment.newInstance()
+                openFragment(moreFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(onNavigationItemReselectedListener)
+
+        val favoritesFragment = FavoritesFragment.newInstance()
+        openFragment(favoritesFragment)
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
