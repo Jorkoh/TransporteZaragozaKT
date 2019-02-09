b7d8af76699a6c4b1c0ff87b02368022d599a3d9
Jorkoh <kohru95@gmail.com>
2019-02-09 01:24:13 +0100

    Added temporal fragment transition animations and extracted navigation stuff to utils file

---
+++ app/src/main/java/com/jorkoh/transportezaragozakt/Activities/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.jorkoh.transportezaragozakt.Activities
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jaredrummler.cyanea.app.CyaneaAppCompatActivity
+import com.jaredrummler.cyanea.prefs.CyaneaSettingsFragment
+import com.jorkoh.transportezaragozakt.Destinations
+import com.jorkoh.transportezaragozakt.R
+import com.jorkoh.transportezaragozakt.ViewModels.MainActivityViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+/**
+ * val args = Bundle()
+ * args.putString(FavoritesFragment.STOP_ID_KEY, "tuzsa-3063")
+ * favoritesFragment.arguments = args
+ */
+
+
+class MainActivity : CyaneaAppCompatActivity() {
+    private val mainActivityVM: MainActivityViewModel by viewModel()
+
+    private val myBackStack
+        get() = mainActivityVM.getCustomBackStack()
+
+    private val onBackPressedCallback = OnBackPressedCallback {
+        if (needsCustomBackHandling(myBackStack, supportFragmentManager)) {
+            goBackToPreviousDestination(
+                myBackStack,
+                supportFragmentManager,
+                R.id.fragment_container,
+                bottom_navigation.menu
+            )
+            return@OnBackPressedCallback true
+        } else {
+            return@OnBackPressedCallback false
+        }
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        val destination = when (item.itemId) {
+            R.id.navigation_favorites -> Destinations.Favorites
+            R.id.navigation_map -> Destinations.Map
+            R.id.navigation_search -> Destinations.Search
+            R.id.navigation_more -> Destinations.More
+            else -> return@OnNavigationItemSelectedListener false
+        }
+        openDestination(
+            destination,
+            myBackStack,
+            supportFragmentManager,
+            R.id.fragment_container
+        )
+        return@OnNavigationItemSelectedListener true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        addOnBackPressedCallback(onBackPressedCallback)
+        bottom_navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        openDestination(
+            myBackStack.lastOrNull() ?: Destinations.getMainDestination(),
+            myBackStack,
+            supportFragmentManager,
+            R.id.fragment_container
+        )
+    }
+
+    fun openThemeSettings(@Suppress("UNUSED_PARAMETER") v: View) {
+        val transaction = supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                android.R.anim.fade_in,
+                android.R.anim.fade_out,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+        val currentFragment = supportFragmentManager.findFragmentByTag(myBackStack.last().getTag())
+        if (currentFragment != null) {
+            transaction.detach(currentFragment)
+        }
+        transaction.add(R.id.fragment_container, CyaneaSettingsFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/jorkoh/transportezaragozakt/Activities/NavigationUtils.kt
@@ -0,0 +1,88 @@
+package com.jorkoh.transportezaragozakt.Activities
+
+import android.view.Menu
+import androidx.fragment.app.FragmentManager
+import com.jorkoh.transportezaragozakt.Destinations
+
+fun needsCustomBackHandling(
+    myBackStack: MutableList<Destinations>,
+    supportFragmentManager: FragmentManager
+): Boolean {
+    return supportFragmentManager.backStackEntryCount == 0 && myBackStack.count() > 1 && !isDoubleHome(myBackStack)
+}
+
+private fun isDoubleHome(myBackStack: MutableList<Destinations>): Boolean {
+    return myBackStack.size == 2
+            && myBackStack[myBackStack.size - 2] == myBackStack.last()
+            && myBackStack.last() == Destinations.getMainDestination()
+}
+
+fun goBackToPreviousDestination(
+    myBackStack: MutableList<Destinations>,
+    supportFragmentManager: FragmentManager,
+    fragmentContainerID: Int,
+    menu: Menu
+) {
+    val destination = myBackStack[myBackStack.size - 2]
+    val currentFragment = supportFragmentManager.findFragmentByTag(myBackStack.last().getTag())
+    val fragmentToOpen = supportFragmentManager.findFragmentByTag(destination.getTag())
+    val transaction = supportFragmentManager.beginTransaction()
+        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+    // Hide current if it exists
+    if (currentFragment != null) {
+        transaction.hide(currentFragment)
+    }
+    // Show the fragment to open
+    if (fragmentToOpen != null) {
+        transaction.show(fragmentToOpen)
+    } else {
+        // Get an instance if it somehow doesn't exists
+        transaction.add(fragmentContainerID, destination.getFragment(), destination.getTag())
+    }
+    // Remove the destination from the custom BackStack
+    myBackStack.removeAt(myBackStack.size - 1)
+    // Manually check the destination on the bottom navigation bar
+    menu.findItem(destination.getMenuItemID()).isChecked = true
+    // Commit the transaction
+    transaction.setPrimaryNavigationFragment(fragmentToOpen)
+        .setReorderingAllowed(true)
+        .commit()
+}
+
+fun openDestination(
+    destination: Destinations,
+    myBackStack: MutableList<Destinations>,
+    supportFragmentManager: FragmentManager,
+    fragmentContainerID: Int
+) {
+    val currentFragment = supportFragmentManager.findFragmentByTag(myBackStack.lastOrNull()?.getTag())
+    var fragmentToOpen = supportFragmentManager.findFragmentByTag(destination.getTag())
+    val transaction = supportFragmentManager.beginTransaction()
+        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+    // Ignore reselection of shown fragment
+    if (currentFragment == fragmentToOpen && fragmentToOpen != null) {
+        return
+    }
+    // Hide the current fragment if it exists
+    if (currentFragment != null) {
+        transaction.hide(currentFragment)
+    }
+    // Show the fragment to Open
+    if (fragmentToOpen == null) {
+        // Get an instance if it's the first time the destination is selected
+        transaction.add(fragmentContainerID, destination.getFragment(), destination.getTag())
+    } else {
+        transaction.show(fragmentToOpen)
+    }
+    // Remove previous instance of this destination if exists, avoid dropping first destination like YT app
+    val position = myBackStack.drop(1).indexOf(destination)
+    if (position != -1) {
+        myBackStack.removeAt(position + 1)
+    }
+    // Add the destination to the custom BackStack
+    myBackStack.add(destination)
+    // Commit the transaction
+    transaction.setPrimaryNavigationFragment(fragmentToOpen)
+        .setReorderingAllowed(true)
+        .commit()
+}
