f01e54827235f06490731c1ffb67ab956a66e01b
Jorkoh <kohru95@gmail.com>
2019-03-13 23:19:33 +0100

    Added tram support for stops update system

--- app/src/main/java/com/jorkoh/transportezaragozakt/tasks/UpdateDataWorker.kt
+++ app/src/main/java/com/jorkoh/transportezaragozakt/tasks/UpdateDataWorker.kt
@@ -25,7 +25,6 @@ class UpdateDataWorker(appContext: Context, workerParams: WorkerParameters) :
         @JvmStatic
         fun enqueueWorker() {
             val constraints = Constraints.Builder()
-                .setRequiresCharging(true)
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build()
 
@@ -45,22 +44,35 @@ class UpdateDataWorker(appContext: Context, workerParams: WorkerParameters) :
 
         // Do the work here
         checkBusStopsUpdate()
-//        checkTramStopUpdate()
+        checkTramStopsUpdate()
 
         // Indicate whether the task finished successfully with the Result
         return Result.success()
     }
 
-    //TODO: Replace this with the actual work
     private fun checkBusStopsUpdate() {
         with(ParseQuery.getQuery<ParseObject>(applicationContext.getString(R.string.parse_bus_stops_collection))) {
             orderByDescending("version")
             limit = 1
             getFirstInBackground { busStopDocument, e ->
                 if (e != null) {
-                    Log.d("Testing stuff", "Retrieving new stops failed", e)
+                    Log.d("Testing stuff", "Retrieving new bus stops failed", e)
                 } else {
                     checkBusStopsUpdate(busStopDocument)
+                }
+            }
+        }
+    }
+
+    private fun checkTramStopsUpdate() {
+        with(ParseQuery.getQuery<ParseObject>(applicationContext.getString(R.string.parse_tram_stops_collection))) {
+            orderByDescending("version")
+            limit = 1
+            getFirstInBackground { tramStopDocument, e ->
+                if (e != null) {
+                    Log.d("Testing stuff", "Retrieving new tram stops failed", e)
+                } else {
+                    checkTramStopsUpdate(tramStopDocument)
                 }
             }
         }
@@ -85,38 +97,64 @@ class UpdateDataWorker(appContext: Context, workerParams: WorkerParameters) :
         }
     }
 
+    private fun checkTramStopsUpdate(tramStopDocument: ParseObject) {
+        if (!isNewTramVersion(tramStopDocument)) {
+            Log.d("Testing stuff", "Tram stops data is up to date")
+        } else {
+            updateTramStops(tramStopDocument)
+
+            val notificationManager: NotificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val builder = NotificationCompat.Builder(applicationContext, "TestingStuff")
+                .setSmallIcon(R.drawable.ic_bus)
+                .setContentTitle("Updated tram stops")
+                .setContentText("New version: ${tramStopDocument.getInt("version")}")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            notificationManager.notify(1, builder.build())
+        }
+    }
+
     private fun isNewBusVersion(busStopDocument: ParseObject): Boolean {
         val sharedPreferences = applicationContext.getSharedPreferences(
             applicationContext.getString(R.string.preferences_version_number_key),
             Context.MODE_PRIVATE
         )
-
         return busStopDocument.getInt("version") > sharedPreferences.getInt(
             applicationContext.getString(R.string.saved_bus_version_number_key),
             1
         )
     }
 
+    private fun isNewTramVersion(tramStopDocument: ParseObject): Boolean {
+        val sharedPreferences = applicationContext.getSharedPreferences(
+            applicationContext.getString(R.string.preferences_version_number_key),
+            Context.MODE_PRIVATE
+        )
+        return tramStopDocument.getInt("version") > sharedPreferences.getInt(
+            applicationContext.getString(R.string.saved_tram_version_number_key),
+            1
+        )
+    }
+
     private fun updateBusStops(busStopDocument: ParseObject) {
         val busStopEntities = mutableListOf<Stop>()
-        busStopDocument.getJSONArray("stops")?.let { stopsJson ->
-            for (i in 0 until stopsJson.length()) {
-                val stopJson = stopsJson.getJSONObject(i)
-                val location = stopJson.getJSONArray("location")?.let { locationJsonArray ->
-                    LatLng(locationJsonArray[1] as Double, locationJsonArray[0] as Double)
-                }
-                val stopEntity = Stop(
-                    StopType.BUS,
-                    checkNotNull(stopJson.getString("id")),
-                    checkNotNull(stopJson.getString("title")),
-                    checkNotNull(location),
-                    false
-                )
-
-                busStopEntities.add(stopEntity)
+        val stopsJson = checkNotNull(busStopDocument.getJSONArray("stops"))
+        for (i in 0 until stopsJson.length()) {
+            val stopJson = stopsJson.getJSONObject(i)
+            val location = stopJson.getJSONArray("location")?.let { locationJsonArray ->
+                LatLng(locationJsonArray[1] as Double, locationJsonArray[0] as Double)
             }
+            val stopEntity = Stop(
+                StopType.BUS,
+                checkNotNull(stopJson.getString("id")),
+                checkNotNull(stopJson.getString("title")),
+                checkNotNull(location),
+                false
+            )
+            busStopEntities.add(stopEntity)
         }
-
         executors.diskIO().execute {
             stopsDao.updateStops(busStopEntities)
         }
@@ -129,6 +167,40 @@ class UpdateDataWorker(appContext: Context, workerParams: WorkerParameters) :
             putInt(
                 applicationContext.getString(R.string.saved_bus_version_number_key),
                 busStopDocument.getInt("version")
+            )
+            apply()
+        }
+    }
+
+    private fun updateTramStops(tramStopDocument: ParseObject) {
+        val tramStopEntities = mutableListOf<Stop>()
+        val stopsJson = checkNotNull(tramStopDocument.getJSONArray("stops"))
+        for (i in 0 until stopsJson.length()) {
+            val stopJson = stopsJson.getJSONObject(i)
+            val location = stopJson.getJSONArray("location")?.let { locationJsonArray ->
+                LatLng(locationJsonArray[1] as Double, locationJsonArray[0] as Double)
+            }
+            val stopEntity = Stop(
+                StopType.TRAM,
+                checkNotNull(stopJson.getString("id")),
+                checkNotNull(stopJson.getString("title")),
+                checkNotNull(location),
+                false
+            )
+            tramStopEntities.add(stopEntity)
+        }
+        executors.diskIO().execute {
+            stopsDao.updateStops(tramStopEntities)
+        }
+
+        val sharedPreferences = applicationContext.getSharedPreferences(
+            applicationContext.getString(R.string.preferences_version_number_key),
+            Context.MODE_PRIVATE
+        )
+        with(sharedPreferences.edit()) {
+            putInt(
+                applicationContext.getString(R.string.saved_tram_version_number_key),
+                tramStopDocument.getInt("version")
             )
             apply()
         }
