07bbb93239af8fd36430a8b6c3acfd1c0ddaf477
Jorkoh <kohru95@gmail.com>
2019-07-15 01:29:33 +0200

    Figured out a less hacky way to handle toolbar menu items on search destination

--- app/src/main/java/com/jorkoh/transportezaragozakt/destinations/search/SearchFragment.kt
+++ app/src/main/java/com/jorkoh/transportezaragozakt/destinations/search/SearchFragment.kt
@@ -17,7 +17,6 @@ import com.jorkoh.transportezaragozakt.MainActivity
 import com.jorkoh.transportezaragozakt.R
 import com.jorkoh.transportezaragozakt.db.StopType
 import com.jorkoh.transportezaragozakt.repositories.util.observeOnce
-import kotlinx.android.synthetic.main.main_container.*
 import kotlinx.android.synthetic.main.search_destination.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
@@ -26,15 +25,14 @@ class SearchFragment : Fragment() {
 
     private val searchVM: SearchViewModel by sharedViewModel()
 
-    private val searchTabPositionObserver = Observer<Int> { position ->
-        search_tab_layout.getTabAt(position)?.select()
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         searchVM.init()
-        searchVM.getSearchTabPosition().observeOnce(viewLifecycleOwner, searchTabPositionObserver)
-        setupToolbar()
+        searchVM.getSearchTabPosition().observeOnce(viewLifecycleOwner, Observer { position ->
+            search_tab_layout.getTabAt(position)?.select()
+        })
+
         search_viewpager.adapter = SearchPagerAdapter(childFragmentManager, requireContext())
         search_tab_layout.setupWithViewPager(search_viewpager)
         search_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
@@ -47,72 +45,52 @@ class SearchFragment : Fragment() {
                 searchVM.setSearchTabPosition(tab.position)
             }
         })
+
+        handleDeepLinks()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Enables onCreateOptionsMenu() callback on activity recreation
-        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.search_destination, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        requireActivity().intent = requireActivity().intent.apply {
-            dataString?.let { uri ->
-                if (uri.startsWith("launchTZ://viewStop")) {
-                    val splitUri = uri.split("/")
-                    findNavController().navigate(
-                        SearchFragmentDirections.actionSearchToStopDetails(
-                            splitUri[3],
-                            splitUri[4]
-                        )
-                    )
-                    data = Uri.EMPTY
-                }
-            }
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Enables onCreateOptionsMenu() callback
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        setupToolbar()
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    //Has to be called from both onCreateView() and onCreateOptionsMenu() to avoid problems with bottom navigation
-    private fun setupToolbar() {
-        requireActivity().main_toolbar?.let { toolbar ->
-            (toolbar.menu.findItem(R.id.item_search)?.actionView as SearchView?)?.setOnQueryTextListener(null)
-            toolbar.menu.clear()
-            toolbar.inflateMenu(R.menu.search_destination_menu)
-            (toolbar.menu.findItem(R.id.item_search).actionView as SearchView).apply {
-                queryHint = getString(R.string.search_view_hint)
-                findViewById<TextView>(androidx.appcompat.R.id.search_src_text).textSize = 16f
-                setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String?): Boolean {
-                        return false
-                    }
-
-                    override fun onQueryTextChange(newText: String?): Boolean {
-                        if (!this@apply.isIconified && isVisible) {
-                            searchVM.query.value = newText
-                        }
-                        return false
-                    }
-                })
-                imeOptions = EditorInfo.IME_ACTION_DONE
-                if (!searchVM.query.value.isNullOrEmpty()) {
-                    // This entire mess is ridiculous, this still doesn't work for orientation changes and the
-                    // workarounds are worse than the problem. It already is hacky enough as it is
-                    // https://github.com/cbeyls/fosdem-companion-android/issues/16
-                    toolbar.menu.findItem(R.id.item_search).expandActionView()
-                    setQuery(searchVM.query.value, false)
-                    clearFocus()
+        inflater.inflate(R.menu.search_destination_menu, menu)
+        (menu.findItem(R.id.item_search).actionView as SearchView).apply {
+            queryHint = getString(R.string.search_view_hint)
+            findViewById<TextView>(androidx.appcompat.R.id.search_src_text).textSize = 16f
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
                 }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (!this@apply.isIconified && isVisible) {
+                        searchVM.query.value = newText
+                    }
+                    return false
+                }
+            })
+            imeOptions = EditorInfo.IME_ACTION_DONE
+            if (!searchVM.query.value.isNullOrEmpty()) {
+                // This entire mess is ridiculous, this still doesn't work for orientation changes and the
+                // workarounds are worse than the problem. It already is hacky enough as it is
+                // https://github.com/cbeyls/fosdem-companion-android/issues/16
+                menu.findItem(R.id.item_search).expandActionView()
+                setQuery(searchVM.query.value, false)
+                clearFocus()
             }
         }
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -144,6 +122,26 @@ class SearchFragment : Fragment() {
                 )
             } else {
                 (requireActivity() as MainActivity).makeSnackbar(getString(R.string.qr_not_recognized))
+            }
+        }
+    }
+
+    // Handles opening stops information directly from notifications or shortcuts
+    private fun handleDeepLinks() {
+        // Jetpack's Navigation doesn't quite work with the multi-graph setup
+        // required for bottom navigation with multi-stack so it's handled manually here
+        requireActivity().intent = requireActivity().intent.apply {
+            dataString?.let { uri ->
+                if (uri.startsWith("launchTZ://viewStop")) {
+                    val splitUri = uri.split("/")
+                    findNavController().navigate(
+                        SearchFragmentDirections.actionSearchToStopDetails(
+                            splitUri[3],
+                            splitUri[4]
+                        )
+                    )
+                    data = Uri.EMPTY
+                }
             }
         }
     }
